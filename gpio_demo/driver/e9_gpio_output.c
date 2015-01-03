58d691ae153e505ccfc1d679cd1bb54f6dd38ea4
root <root@ubuntu.(none)>
2015-01-03 15:40:25 +0800

    add comments and recompile

--- gpio_demo/driver/e9_gpio_output.c
+++ gpio_demo/driver/e9_gpio_output.c
@@ -1,3 +1,12 @@
+/*
+ * Driver for gpio output on E9 board.
+ *
+ * Copyright 2014 Jone Yim
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
